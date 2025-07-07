f878ddcdaed659373393b8176348a688a0e50995
Ryan-D-Gast <ryguy10202003@gmail.com>
2025-07-07 18:53:06 -0500

    Update series.rs

--- src/series.rs
+++ src/series.rs
@@ -1,7 +1,7 @@
 use crate::{Interpolation, Line, Marker};
 use bon::Builder;
 
-#[derive(Builder)]
+#[derive(Clone, Builder)]
 pub struct Series<'a, T = f32> {
     pub data: Vec<(T, T)>,
     #[builder(default = "")]
@@ -18,4 +18,10 @@ pub struct Series<'a, T = f32> {
     pub line_width: f32,
     #[builder(default = Interpolation::Linear)]
     pub interpolation: Interpolation,
+}
+
+impl<'a, T> Default for Series<'a, T> {
+    fn default() -> Self {
+        Series::builder().data(vec![]).build()
+    }
 }
